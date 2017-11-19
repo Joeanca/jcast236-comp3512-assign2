172c8fc12a8e8aec01498db85a08dbfff2542d7b
Jorge <jcast236@mtroyal.ca>
2017-11-19 03:56:26 +0000

    logout session destruction working again, sorry guys

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
+<?php
+ session_start();
+    if( strcasecmp($_SERVER['REQUEST_METHOD'],"POST") === 0) {
+         $_SESSION['postdata'] = $_POST;
+        header("Location: ".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']);
+    exit;
+    }
+    if(isset($_SESSION['UserID'])){
+        session_destroy();
+    }if
+    ( isset($_SESSION['postdata'])) {
+        $_POST = $_SESSION['postdata'];
+    unset($_SESSION['postdata']);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
